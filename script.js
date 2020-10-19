b9cfcbad2d507f5b566a40cf5cb9792ddf382871
Joe Wickes <joe.c.wickes@gmail.com>
2020-10-19 15:22:23 -0400

    Finish adding click functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $(function() {
-
+  // On Click
+  $('.thumbnails').on('click', function(e) {
+    e.preventDefault();
+    $('.hero img').attr('src', $(e.target).attr('src')).attr('alt', $(e.target).attr('alt'));
+  });
 });
